4034499268ca190ce5b3b2893797590a68c417fc
ad5960 <84094924+ad5960@users.noreply.github.com>
2021-12-14 15:18:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,63 +1,237 @@
 
-      var fileList;
-      document.getElementById("myfile").onchange = function (event) {
-        fileList = event.target.files;}
-document.getElementById("contactForm").addEventListener("submit", submitForm);
+let apiKey = '32c79f457e54408e98960272cc4c0e08';
 
-function submitForm(e) {
-  
-  e.preventDefault();
-  let maxSize = 10*1024*1024;
-if(fileList[0].size<=maxSize){
 
-    let name = getInputVal("fullname");
-    let email = getInputVal("email");
-    let number = getInputVal("number");
-    let event = getInputVal("events");
+function kodak(element){
+
+  var db = firebase.firestore();
+  let val = element.getAttribute("value");
+  let temp = 1;
+  $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+  let ip = data.ip_address;
   
-    var db = firebase.firestore();
-    // Add a new document in collection "cities"
-    db.collection("contact")
-      .doc(email)
-      .set({
-        name: name,
-        email: email,
-        number: number,
-        event: event,
-      })
-      .then(() => {
-        console.log("Document successfully written!");
-        alert("Submission Successful");
-        
-  
-          //TODO do something with fileList.
-          // Get a reference to the storage service, which is used to create references in your storage bucket
-        //   var storage = firebase.storage();
-        
-          // Create a storage reference from our storage service
-          console.log(fileList[0]);
-          var storage = firebase.storage();
-          var storageRef = storage.ref();
-          let extension = fileList[0].name.split('.').pop();
-          let uploadFileName =  `${number}.${extension}`
-        //   console.log(extension)
-          var childRef = storageRef.child(uploadFileName);
-          // 'file' comes from the Blob or File API
-          childRef.put(fileList[0]).then((snapshot) => {
-            console.log("Uploaded a blob or file!");
-            window.location = 'https://srmmunsoc.com/';
-          });
+  db.collection("kodak")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            
+            if(doc.id == ip)
+            {
+              temp = 0
+              alert("You have already voted")
+            }
+        });
+
+        if( temp == 1 )
+        {
           
-      })
-      
-      .catch((error) => {
-        console.error("Error writing document: ", error);
-      });
+
+          db.collection("kodak")
+              .doc(ip)
+              .set({
+                ip: ip,
+                choice: val 
+              })
+              alert("Thank you for voting")
+        }
+
+        else{
+          
+          temp = 1;
+          
+        }
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  })
 }
-else{
-    alert('File too big')
+
+function guten(element){
+
+  var db = firebase.firestore();
+  let val = element.getAttribute("value");
+  let temp = 1;
+  $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+  let ip = data.ip_address;
+  
+  db.collection("guten")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            
+            if(doc.id == ip)
+            {
+              temp = 0
+              alert("You have already voted")
+            }
+        });
+
+        if( temp == 1 )
+        {
+          
+
+          db.collection("guten")
+              .doc(ip)
+              .set({
+                ip: ip,
+                choice: val 
+              })
+              alert("Thank you for voting")
+        }
+
+        else{
+          
+          temp = 1;
+          
+        }
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  })
 }
+
+function worth(element){
+
+  var db = firebase.firestore();
+  let val = element.getAttribute("value");
+  let temp = 1;
+  $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+  let ip = data.ip_address;
+  
+  db.collection("worth")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            
+            if(doc.id == ip)
+            {
+              temp = 0
+              alert("You have already voted")
+            }
+        });
+
+        if( temp == 1 )
+        {
+          
+          db.collection("worth")
+              .doc(ip)
+              .set({
+                ip: ip,
+                choice: val 
+              })
+              alert("Thank you for voting")
+        }
+
+        else{
+          
+          temp = 1;
+          
+        }
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  })
 }
-function getInputVal(id) {
-  return document.getElementById(id).value;
+
+function attack(element){
+
+  var db = firebase.firestore();
+  let val = element.getAttribute("value");
+  let temp = 1;
+  $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+  let ip = data.ip_address;
+  
+  db.collection("attack")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            
+            if(doc.id == ip)
+            {
+              temp = 0
+              alert("You have already voted")
+            }
+        });
+
+        if( temp == 1 )
+        {
+          
+
+          db.collection("attack")
+              .doc(ip)
+              .set({
+                ip: ip,
+                choice: val 
+              })
+              alert("Thank you for voting")
+        }
+
+        else{
+          
+          temp = 1;
+          
+        }
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  })
+}
+
+function sketch(element){
+
+  var db = firebase.firestore();
+  let val = element.getAttribute("value");
+  let temp = 1;
+  $.getJSON('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey, function(data) {
+    
+  let ip = data.ip_address;
+  
+  db.collection("sketch")
+    .get()
+    .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            
+            if(doc.id == ip)
+            {
+              temp = 0
+              alert("You have already voted")
+            }
+        });
+
+        if( temp == 1 )
+        {
+          
+
+          db.collection("sketch")
+              .doc(ip)
+              .set({
+                ip: ip,
+                choice: val 
+              })
+              alert("Thank you for voting")
+        }
+
+        else{
+          
+          temp = 1;
+          
+        }
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+  })
 }
